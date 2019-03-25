df5f15a063a8b28bb26f78745d402ce71b6f3a71
KevinDCrowe <46440712+KevinDCrowe@users.noreply.github.com>
2019-03-25 01:07:54 -0700

    Update DataProtocols.c

--- DataProtocols.c
+++ DataProtocols.c
@@ -28,7 +28,53 @@ uartr(int buffer[M],int baudrate,bool parity, int stopbits) {
 		sleep(stopbits / baudrate);
 	}
 }
-spi(){}
+spi(){//master
+int clk;
+int freqc;
+int mI;
+int mO;
+int ss_1;
+//...
+int ss_n;
+int instruction[][];//list containing all possible device adresses
+int rw;
+int address[][];//list containing all possible byte adresses
+int data_0[W][];
+//...
+int data_n[W][];
+int buffer;
+int mode
+//generate clock signal (seperate processor ideally)
+while(1){
+set(clk)=1;
+sleep(.5/freqc);
+set(clk)=0;
+sleep(.5/freqc);
+}
+//read or write
+//load instruction
+int *instuctionb = (int *) malloc(sizeof(int)*W)
+//load address 
+int *addressb = (int *) malloc(sizeof(int)*W);
+//load data for recieve/transmit
+int *datab = (int *) malloc(sizeof(int)*W);
+//set cs_n to 0
+//send instruction
+//send adress
+//send/recieve data
+//set cs to 1
+
+
+for (int j=0;j<200;j++) //send data
+for (int i=0;i<W;i++){
+if (data[i][j]==1){
+set(mO)=1;
+}else{
+set(mO)=0;
+}
+}
+
+}
 usb(){}
 i2c(){}
 
