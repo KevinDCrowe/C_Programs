a5eea68180a86586d1484fdfc16e632705a09a7e
KevinDCrowe <46440712+KevinDCrowe@users.noreply.github.com>
2019-03-26 12:49:11 -0700

    Update and rename Direct_2_filter(incomplete).c to Direct_2_filter.c

---
+++ Direct_2_filter.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define a1 10
+#define D 1024
+
+D2Filter(int a[3],int b[3],int x[D]){
+
+int v[3]={0};
+int *y=(int *)malloc(sizeof(int)*D);
+for(int i=0;i<D;i++){
+	y[i]=0;
+}
+	
+v[i]=x[i]+a[1]*v[i+1]+a[2]*v[i+2];
+y[i]=v[j]*b[j];
+
+
+for (int i = 0; i < D; i++) {
+	//"Delays" Accumulator entries
+	for (int j = 1; j < 3; j++) {
+		v[j] = v[j -1];
+      	}
+	//Zeros current Accumulator value
+	v[0]=0;
+	//Performs one round of filtering
+	v[i]=x[i]+a[1]*v[i+1]+a[2]*v[i+2];
+	for( int j=0;j<3;j++){
+		y[i]=v[j]*b[j]+y[i];
+	}      
+}
+}
