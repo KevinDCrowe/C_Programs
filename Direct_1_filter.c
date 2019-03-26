fb8a2ef547319d212c7f45655583b5d108b1eb16
KevinDCrowe <46440712+KevinDCrowe@users.noreply.github.com>
2019-03-26 14:14:55 -0700

    Update and rename Direct_1_filter(incomplete).c to Direct_1_filter.c

--- Direct_1_filter.c
+++ Direct_1_filter.c
@@ -2,13 +2,11 @@
 #include <stdio.h>
 #include <math.h>
 
-#define q 9 //Data scaling factor
-#define D 1024 //Size of target data
-
-
-d1filter(int a[3],int b[3],int x[D]) {
+d1filter(int a[3],int b[3],int x[],int q) {
+	D=sizeof(x)/sizeof(int);
 	//Allocates memory for the Accumulator
 	int *y = (int *)malloc(sizeof(int)*(D)); //output
+	int v[3]={0}; //Accumulator buffer
 	
 	//Scales the input data
 	for (int i = 0; i < 3; i++) {
@@ -16,23 +14,18 @@ d1filter(int a[3],int b[3],int x[D]) {
 	}
 	//Performs the filtering
 	for (int i = 0; i < D; i++) {
-		//Fills buffer for each time delay i
-		for (int j = 0; j < 3 && j < (i + 1); j++) {
-			c[j] = x[i - j];
-		}
+		
 		//"Delays" Accumulator entries
 		for (int j = 1; j < 3; j++) {
-			d[j] = d[j -1];
+			v[j] = v[j -1];
 		}
 		//Zeros current Accumulator value
-		d[0] = 0;
+		v[0] = 0;
 		//Performs one round of filtering
-		d[0] = x[0]*b[0];
+		v[0] = x[0]*b[0];
 		for (int j = 1; j < 3; j++) {
-			d[0] = d[0] + x[j]*b[j] + a[j]*d[j];
+			v[0] = v[0] + x[j]*b[j] - a[j]*v[j];
 		}
-		y[i] = d[0] / pow(2, q);
-		
-		
+		y[i] = v[0] / pow(2, q);
 	}
 }
