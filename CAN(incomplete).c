1da3f190a13e1382ff31ad7b9073ecef791992c0
KevinDCrowe <46440712+KevinDCrowe@users.noreply.github.com>
2019-04-11 14:17:31 -0700

    Update CAN(incomplete).c

--- CAN(incomplete).c
+++ CAN(incomplete).c
@@ -13,89 +13,50 @@ read(canH,canL){
   return boool;
 }
 
-CAN(){
-  int devAd[11];
-  
-    
-//arbitration
-  for(int i=0;i<11;i++){
-  
-  }
-}
 CANr(){
+  
+CANt(){
   
  "init I/O pins"(return canL & canH)
 //A(0),B(1), or FD(2)
 int typ=0;
   
 for(int n=0;n<100;n++){
-boool = read(canL,canH);
-if(n<3){"SoF"}elseif(){}elseif(){}elseif(){}elseif(){}else{}
+  
+  boool = read(canL,canH);
+  
+  if(n<1){
+    //"SoF"
+    set(0)
+  }elseif(n>=1 && n<12){
+    //Arbitration
+  }elseif(n>=13 && n<14){
+    //request remote
+   if(boool==1){}else{};
+  }elseif(n>=14 && n<16){
+    //ID ext and Reserved
+    set(0)
+  }elseif(n>=16 && n<20){
+    //Data Length
+    num=read(canH,canL)
+    cntr = cntr + pow(2,c)×num
+  }elseif(n>=20 && n<8*num+20){
+    //"Data"
+  }elseif(n>=8*num+20 && n<8*num+36){
+    //"CRC"
+  }elseif(n>=8*num+36 && n<8*num+38){
+    //"Ack"
+  }elseif(n>=8*num+38 && n<8*num+48){
+    //"EoF"
+    Set(1);  
+  }else{n=0}
 }  
 
  
-  //arbitration
-   for(int i=0;i<11;i++){
-   
-   }
-  //request remote
-   if(boool==1){}else{};
-  //control
-   for(int i=0;i<4;i++){//Determines data size
-   int Q=Q+boool[i]*pow(2,i);
-   }
-  //data
-   for(int i=0;i<Q;i++){//reads data
-   data[i]=boool[i]
-   }
-  //CRC
- }
-}
+  
+  
 
- Int data[d][g];
-Int devAd[b];
 
-Int a=;//size of SoF
-Int b=;//size of arbitration
-Int c=;//size of control
-Int d=;//size of data
-Int e=;//size of crc
-Int f=;//size of EoF
-int g=;//number of data entries
-Int n=0;
 
-For(int i=0;i<a;i++)
-Set(1);
-n++
-}\\start of frame
 
-For(int i=0;i<b;i++){
-set(0)
-read(boool)
-if(boool==1){
-continue
-}else{
-"shutoff"}
-n++
-}\\decision
-
-For(int i=0;i<c;i++){
-num=read(d[n])
-cntr=pow(2,c)×num +ctnr
-n++
-}\\control
-
-For(int i=0;i<num;i++){
-mem[n]=read(d[n])
-n++
-}\\data
-
-For(int i=0;i<e;i++){
-n++
-}\\crc
-
-For(int i=0;i<f;i++){
-set(1)
-n++
-}\\EoF
 
